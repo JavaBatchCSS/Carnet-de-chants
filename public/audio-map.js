1f066502f91e69a9ce0c35080a27d2c099384fd6
JavaBatchCSS <153531121+JavaBatchCSS@users.noreply.github.com>
2026-05-24 16:28:58 +0200

    Add files via upload

---
+++ public/audio-map.js
@@ -0,0 +1,1132 @@
+window.audio_map_data = {
+  "1": [
+    {
+      "title": "Notre Patrie",
+      "youtubeId": "NONE"
+    }
+  ],
+  "2": [
+    {
+      "title": "Prière des chefs",
+      "youtubeId": "NONE"
+    }
+  ],
+  "3": [
+    {
+      "title": "Aux Scouts morts pour la France",
+      "youtubeId": "NONE"
+    }
+  ],
+  "4": [
+    {
+      "title": "Prière des chevaliers",
+      "youtubeId": "NONE"
+    }
+  ],
+  "6": [
+    {
+      "title": "Prière Scoute",
+      "youtubeId": "kTYlxC2Dms4"
+    }
+  ],
+  "7": [
+    {
+      "title": "Cœur de Jésus",
+      "youtubeId": "prqOrcS7yu4"
+    }
+  ],
+  "9": [
+    {
+      "title": "Voguons au vent de mer",
+      "youtubeId": "NZ0H7JVZKy4"
+    }
+  ],
+  "10": [
+    {
+      "title": "Chant de la promesse",
+      "youtubeId": "fgGdUYGDAvc"
+    }
+  ],
+  "11": [
+    {
+      "title": "Cantique des patrouilles",
+      "youtubeId": "o49k9mQ-jGw"
+    }
+  ],
+  "12": [
+    {
+      "title": "Miserere de la mer",
+      "youtubeId": "iZKDf1QPu50"
+    }
+  ],
+  "13": [
+    {
+      "title": "À Jeanne d'Arc",
+      "youtubeId": "o8LENXh0AoQ"
+    }
+  ],
+  "14": [
+    {
+      "title": "Notre-Dame des Éclaireurs",
+      "youtubeId": "8_Uu_whgJAU"
+    }
+  ],
+  "15": [
+    {
+      "title": "Notre-Dame des Scouts marins",
+      "youtubeId": "iv_Mcit6ouI"
+    }
+  ],
+  "16": [
+    {
+      "title": "Avant d'aller dormir",
+      "youtubeId": "0KwArLPLFJw"
+    }
+  ],
+  "17": [
+    {
+      "title": "Saint Georges",
+      "youtubeId": "V_Jixpt0jqk"
+    }
+  ],
+  "19": [
+    {
+      "title": "Vierge des chemins de France",
+      "youtubeId": "fMQKV1OiB7M"
+    }
+  ],
+  "21": [
+    {
+      "title": "Mater Alma",
+      "youtubeId": "9K7mxZaKZ40"
+    }
+  ],
+  "22": [
+    {
+      "title": "Je mets ma confiance",
+      "youtubeId": "GegPmWvelFY"
+    }
+  ],
+  "23": [
+    {
+      "title": "Prière du Parachutiste",
+      "youtubeId": "VoSv5MtNHfw"
+    }
+  ],
+  "26": [
+    {
+      "title": "Ô ma troupe",
+      "youtubeId": "ysOU00W8iRk"
+    }
+  ],
+  "27": [
+    {
+      "title": "La Croix des Scouts",
+      "youtubeId": "IjvbWhSqYyY"
+    }
+  ],
+  "28": [
+    {
+      "title": "Les grèves",
+      "youtubeId": "ZpsExxNs_VY"
+    }
+  ],
+  "29": [
+    {
+      "title": "Va, scout de France !",
+      "youtubeId": "tQaGAUhgeEc"
+    }
+  ],
+  "30": [
+    {
+      "title": "Les trois devises",
+      "youtubeId": "Vq2NAmX3FDc"
+    }
+  ],
+  "31": [
+    {
+      "title": "Youkaïdi",
+      "youtubeId": "55RBP6uZ94M"
+    }
+  ],
+  "32": [
+    {
+      "title": "L'appel de la Route",
+      "youtubeId": "A5kRCJxCQyI"
+    }
+  ],
+  "33": [
+    {
+      "title": "Quand le ciel est bleu",
+      "youtubeId": "PeC3ykzCWU4"
+    }
+  ],
+  "34": [
+    {
+      "title": "L'Espérance",
+      "youtubeId": "Bo1s03VUL50"
+    }
+  ],
+  "35": [
+    {
+      "title": "La route est longue",
+      "youtubeId": "hEME0AfWOvQ"
+    }
+  ],
+  "36": [
+    {
+      "title": "Chef de patrouille",
+      "youtubeId": "Ft_K7vs2tBk"
+    }
+  ],
+  "37": [
+    {
+      "title": "Chant des adieux",
+      "youtubeId": "xyH2d4KsJDQ"
+    }
+  ],
+  "38": [
+    {
+      "title": "Je suis un peu fou",
+      "youtubeId": "TVcgqPoCY60"
+    }
+  ],
+  "39": [
+    {
+      "title": "Souvenirs qui passent",
+      "youtubeId": "CS5WZBkU2kw"
+    }
+  ],
+  "40": [
+    {
+      "title": "Ils étaient trois garçons",
+      "youtubeId": "patj37ZYi20"
+    }
+  ],
+  "42": [
+    {
+      "title": "Vieux pèlerin",
+      "youtubeId": "GXSNXAXuI_E"
+    }
+  ],
+  "43": [
+    {
+      "title": "La Bohème",
+      "youtubeId": "-M5ASKCAmzk"
+    }
+  ],
+  "45": [
+    {
+      "title": "Sur les monts",
+      "youtubeId": "9mujOovZHP4"
+    }
+  ],
+  "46": [
+    {
+      "title": "L'appel scout",
+      "youtubeId": "bGESEqw3zbI"
+    }
+  ],
+  "47": [
+    {
+      "title": "Au camp",
+      "youtubeId": "nG37mgA6xCY"
+    }
+  ],
+  "49": [
+    {
+      "title": "La voix des petits",
+      "youtubeId": "XfETHGEMn2Q"
+    }
+  ],
+  "50": [
+    {
+      "title": "L'aventure",
+      "youtubeId": "tTwlSOT1WdI"
+    }
+  ],
+  "51": [
+    {
+      "title": "Le camp nous appelle",
+      "youtubeId": "Jwag7ciQQtc"
+    }
+  ],
+  "52": [
+    {
+      "title": "Être prêt",
+      "youtubeId": "jf15yXYXcqc"
+    }
+  ],
+  "54": [
+    {
+      "title": "Une fleur au chapeau",
+      "youtubeId": "YEwmvfJqjcw"
+    }
+  ],
+  "55": [
+    {
+      "title": "La loi scoute",
+      "youtubeId": "DK6Kj5Vlkgk"
+    }
+  ],
+  "56": [
+    {
+      "title": "Montjoie",
+      "youtubeId": "98all2-U1EY"
+    },
+    {
+      "title": "Sur mon honneur",
+      "youtubeId": "19Sy0OyXk58"
+    }
+  ],
+  "57": [
+    {
+      "title": "L'alphabet scout",
+      "youtubeId": "RkQW7MaGTmA"
+    }
+  ],
+  "59": [
+    {
+      "title": "La marche au camp",
+      "youtubeId": "NQcTgi_KprI"
+    }
+  ],
+  "64": [
+    {
+      "title": "Marchons dans le vent",
+      "youtubeId": "Oy_liDM28QA"
+    }
+  ],
+  "65": [
+    {
+      "title": "Chant de Troupe",
+      "youtubeId": "e1g1Q3lySPk"
+    }
+  ],
+  "66": [
+    {
+      "title": "La joie scoute",
+      "youtubeId": "8xJ6nxw8nUk"
+    }
+  ],
+  "67": [
+    {
+      "title": "Les excuses de l'aspirant",
+      "youtubeId": "p1jvN_wPPd8"
+    }
+  ],
+  "68": [
+    {
+      "title": "Hé ! Garçon",
+      "youtubeId": "n0-qJBZGJPI"
+    }
+  ],
+  "70": [
+    {
+      "title": "Hardi les gars !",
+      "youtubeId": "lzCBUUz3LCA"
+    }
+  ],
+  "72": [
+    {
+      "title": "Le Capitaine de Saint-Malo",
+      "youtubeId": "NlgVNw4mhZg"
+    }
+  ],
+  "73": [
+    {
+      "title": "La Danaé",
+      "youtubeId": "SrgNTZfhLy4"
+    }
+  ],
+  "75": [
+    {
+      "title": "Au 31 du mois d'août",
+      "youtubeId": "WYNy0GM2gu8"
+    }
+  ],
+  "77": [
+    {
+      "title": "Les marins de Groix",
+      "youtubeId": "dBIO2u2zOyY"
+    }
+  ],
+  "82": [
+    {
+      "title": "La fille du coupeur de paille",
+      "youtubeId": "un_xO9RWKbs"
+    }
+  ],
+  "83": [
+    {
+      "title": "Les filles de La Rochelle",
+      "youtubeId": "cxvZLaPf8Fw"
+    }
+  ],
+  "84": [
+    {
+      "title": "Quand je suis parti de La Rochelle",
+      "youtubeId": "_GgfppQZ4U4"
+    }
+  ],
+  "85": [
+    {
+      "title": "Belle Virginie",
+      "youtubeId": "Kg1xySZgGSw"
+    }
+  ],
+  "88": [
+    {
+      "title": "Qui peut faire de la voile sans vent",
+      "youtubeId": "IpBnNK_eVmg"
+    }
+  ],
+  "91": [
+    {
+      "title": "Brave marin",
+      "youtubeId": "V065-EBGzNs"
+    }
+  ],
+  "92": [
+    {
+      "title": "Chantons pour passer le temps",
+      "youtubeId": "_HK2g8dNFSY"
+    }
+  ],
+  "93": [
+    {
+      "title": "Guerre guerre, vente vent !",
+      "youtubeId": "PJxHWQy1bk4"
+    }
+  ],
+  "94": [
+    {
+      "title": "Chant des Corsaires",
+      "youtubeId": "4a4T14L3GTY"
+    }
+  ],
+  "98": [
+    {
+      "title": "Monsieur de Charette",
+      "youtubeId": "Jm29bJ2dlMQ"
+    }
+  ],
+  "99": [
+    {
+      "title": "Chanson de Monsieur Henri",
+      "youtubeId": "by-BACDq1qA"
+    }
+  ],
+  "101": [
+    {
+      "title": "Le petit Grégoire",
+      "youtubeId": "vqViMtEdDPY"
+    }
+  ],
+  "102": [
+    {
+      "title": "Kyrie des gueux",
+      "youtubeId": "PFgQ3XZg6cc"
+    }
+  ],
+  "104": [
+    {
+      "title": "Les bleus sont là",
+      "youtubeId": "pfQafi5crm4"
+    }
+  ],
+  "105": [
+    {
+      "title": "La chasse aux loups",
+      "youtubeId": "leuwMdMMTGQ"
+    }
+  ],
+  "106": [
+    {
+      "title": "Chant du Bas-Poitou",
+      "youtubeId": "6GqDuO0kcQw"
+    }
+  ],
+  "107": [
+    {
+      "title": "La blanche hermine",
+      "youtubeId": "HUphoDbaPgQ"
+    }
+  ],
+  "109": [
+    {
+      "title": "Chanson de l'armée de Charette",
+      "youtubeId": "JPJReD7B5g4"
+    }
+  ],
+  "110": [
+    {
+      "title": "Jean Cottereau",
+      "youtubeId": "CVcY7ty6dwE"
+    }
+  ],
+  "111": [
+    {
+      "title": "Les Chouans",
+      "youtubeId": "bg-REpy5L6s"
+    }
+  ],
+  "112": [
+    {
+      "title": "La Catholique",
+      "youtubeId": "rc-tAVacg4I"
+    }
+  ],
+  "114": [
+    {
+      "title": "La Saint Hubert",
+      "youtubeId": "kAyMvoVotzQ"
+    }
+  ],
+  "116": [
+    {
+      "title": "Les Chacals",
+      "youtubeId": "ngS66txQlZQ"
+    }
+  ],
+  "117": [
+    {
+      "title": "Occident, en avant !",
+      "youtubeId": "uuGlE1Sl1PQ"
+    },
+    {
+      "title": "Au revoir camarade",
+      "youtubeId": "UjBBWtvqpAw"
+    },
+    {
+      "title": "Le Cor",
+      "youtubeId": "wNvVM9xj4fY"
+    }
+  ],
+  "118": [
+    {
+      "title": "Pelot d'Hennebont",
+      "youtubeId": "tq4Dio1yjtU"
+    }
+  ],
+  "119": [
+    {
+      "title": "Malbrough s'en va-t-en guerre",
+      "youtubeId": "4-zmHDjJrWg"
+    }
+  ],
+  "120": [
+    {
+      "title": "Frédéri",
+      "youtubeId": "dWRYGjdY5q4"
+    }
+  ],
+  "121": [
+    {
+      "title": "Le vieux Joe",
+      "youtubeId": "U7cspGkh9k4"
+    }
+  ],
+  "122": [
+    {
+      "title": "Le vieux chalet",
+      "youtubeId": "K1BKIf4rEGM"
+    }
+  ],
+  "123": [
+    {
+      "title": "La bataille de Fontenoy",
+      "youtubeId": "q1Gu5CRV4bI"
+    }
+  ],
+  "124": [
+    {
+      "title": "Fleur d'épine",
+      "youtubeId": "6VbrY2HhFNM"
+    }
+  ],
+  "125": [
+    {
+      "title": "La complainte de Mandrin",
+      "youtubeId": "ZFuDCq-fWsI"
+    }
+  ],
+  "126": [
+    {
+      "title": "La Piémontaise",
+      "youtubeId": "COVxJ_9Qszc"
+    }
+  ],
+  "127": [
+    {
+      "title": "File la laine",
+      "youtubeId": "soCF-0KThsY"
+    }
+  ],
+  "128": [
+    {
+      "title": "Debout les gars",
+      "youtubeId": "KVdxpOqa-Yg"
+    }
+  ],
+  "129": [
+    {
+      "title": "Le Roi Arthur",
+      "youtubeId": "dkiBwTQnAu4"
+    }
+  ],
+  "130": [
+    {
+      "title": "Le soldat belge",
+      "youtubeId": "ngv0qAcfpds"
+    }
+  ],
+  "132": [
+    {
+      "title": "Les gars de Locminé",
+      "youtubeId": "vqfixn08FjM"
+    }
+  ],
+  "133": [
+    {
+      "title": "La trompe sonne",
+      "youtubeId": "TJN43hH902E"
+    }
+  ],
+  "134": [
+    {
+      "title": "Berceuse du petit Prince",
+      "youtubeId": "M4xMg_LI3HM"
+    }
+  ],
+  "135": [
+    {
+      "title": "Le Roy Louis",
+      "youtubeId": "oevTvrit-N0"
+    }
+  ],
+  "136": [
+    {
+      "title": "Saints de France",
+      "youtubeId": "xxEX6B-4Cd8"
+    }
+  ],
+  "137": [
+    {
+      "title": "Russie libre",
+      "youtubeId": "xuty0pFEkzA"
+    }
+  ],
+  "138": [
+    {
+      "title": "Le Seigneur appelle ses militants",
+      "youtubeId": "6f1QsaN_aJU"
+    }
+  ],
+  "139": [
+    {
+      "title": "Le cygne de Montfort",
+      "youtubeId": "XGcd3FJzREc"
+    }
+  ],
+  "140": [
+    {
+      "title": "Honneur et Fidélité",
+      "youtubeId": "iTucDo8sJiE"
+    }
+  ],
+  "141": [
+    {
+      "title": "Le Roy Renaud",
+      "youtubeId": "GLus6tXUOTI"
+    }
+  ],
+  "142": [
+    {
+      "title": "La violette",
+      "youtubeId": "S9wcQI2Irfo"
+    }
+  ],
+  "143": [
+    {
+      "title": "Les Terres Saintes",
+      "youtubeId": "FTveHIDZmMM"
+    }
+  ],
+  "144": [
+    {
+      "title": "Vive Henri IV",
+      "youtubeId": "WVys3aoU4SY"
+    }
+  ],
+  "145": [
+    {
+      "title": "Marche des vagabonds",
+      "youtubeId": "GhUg1RRWjrc"
+    }
+  ],
+  "146": [
+    {
+      "title": "Silvestrick",
+      "youtubeId": "fUjqxEkOHwg"
+    }
+  ],
+  "147": [
+    {
+      "title": "Monsieur de Turenne",
+      "youtubeId": "_me86LtC6oE"
+    }
+  ],
+  "148": [
+    {
+      "title": "Réveillez-vous Picards",
+      "youtubeId": "GG12s6zB_7k"
+    }
+  ],
+  "149": [
+    {
+      "title": "Les Lansquenets",
+      "youtubeId": "9M6_ZQeJs-4"
+    }
+  ],
+  "150": [
+    {
+      "title": "Les Dragons de Noailles",
+      "youtubeId": "Neupp7Ggy6w"
+    }
+  ],
+  "151": [
+    {
+      "title": "Déjà mal mariée",
+      "youtubeId": "TkyIaWnMFhQ"
+    }
+  ],
+  "152": [
+    {
+      "title": "Les Partisans blancs",
+      "youtubeId": "AbrpFagWLDw"
+    }
+  ],
+  "153": [
+    {
+      "title": "La mort de Saint François",
+      "youtubeId": "JQtal7OqyXQ"
+    }
+  ],
+  "154": [
+    {
+      "title": "La légende de Saint Nicolas",
+      "youtubeId": "M9steij7LEM"
+    }
+  ],
+  "155": [
+    {
+      "title": "La légende de Saint Nicolas (suite)",
+      "youtubeId": "U_IEBtOEAQU"
+    }
+  ],
+  "156": [
+    {
+      "title": "Cadet Rousselle",
+      "youtubeId": "1obMzIJ77BA"
+    }
+  ],
+  "157": [
+    {
+      "title": "La laine des moutons",
+      "youtubeId": "m5jHyHAq_uM"
+    }
+  ],
+  "158": [
+    {
+      "title": "Le sire de Framboisy",
+      "youtubeId": "BNDLa2RmyyY"
+    }
+  ],
+  "159": [
+    {
+      "title": "Kergariou",
+      "youtubeId": "2MeqhZdyO0E"
+    }
+  ],
+  "160": [
+    {
+      "title": "La Cavalcade",
+      "youtubeId": "LfUD10OrttY"
+    }
+  ],
+  "163": [
+    {
+      "title": "Les Africains",
+      "youtubeId": "FCX3f7OimZk"
+    }
+  ],
+  "164": [
+    {
+      "title": "Le Combat de demain",
+      "youtubeId": "jCdpk9yA-xw"
+    }
+  ],
+  "165": [
+    {
+      "title": "Les Bérets verts",
+      "youtubeId": "z-AKwgjrXx0"
+    }
+  ],
+  "166": [
+    {
+      "title": "Les Commandos",
+      "youtubeId": "jk1fDHsUV8M"
+    }
+  ],
+  "167": [
+    {
+      "title": "Marche du 1er Commando de France",
+      "youtubeId": "RFpaJA3kEGE"
+    }
+  ],
+  "168": [
+    {
+      "title": "Les Cuirassiers",
+      "youtubeId": "eOGg_gVoujI"
+    }
+  ],
+  "169": [
+    {
+      "title": "Sarie Mares",
+      "youtubeId": "x0JReT_WIRw"
+    }
+  ],
+  "170": [
+    {
+      "title": "Alléluia !",
+      "youtubeId": "jsawpojUSGw"
+    }
+  ],
+  "171": [
+    {
+      "title": "Chant des marais",
+      "youtubeId": "CorFxKgirqI"
+    }
+  ],
+  "172": [
+    {
+      "title": "Marche de la 2ème DB",
+      "youtubeId": "i4v-CgyuEuA"
+    }
+  ],
+  "173": [
+    {
+      "title": "Les oies sauvages",
+      "youtubeId": "vs-vLSuyxbk"
+    }
+  ],
+  "174": [
+    {
+      "title": "La petite piste",
+      "youtubeId": "yyFItQKu38c"
+    }
+  ],
+  "175": [
+    {
+      "title": "Eugénie",
+      "youtubeId": "eJ4HQx9mp7E"
+    }
+  ],
+  "176": [
+    {
+      "title": "La Cavalerie d'Afrique",
+      "youtubeId": "XMktlTdIBgc"
+    }
+  ],
+  "177": [
+    {
+      "title": "Chant du 1er RHP",
+      "youtubeId": "oa8tLZjNGMI"
+    }
+  ],
+  "178": [
+    {
+      "title": "Adieu du bataillon de choc",
+      "youtubeId": "A3gJ7LsxPOY"
+    }
+  ],
+  "179": [
+    {
+      "title": "Contre les Viets",
+      "youtubeId": "8GHSU7s84g8"
+    }
+  ],
+  "180": [
+    {
+      "title": "Ceux du Liban",
+      "youtubeId": "aqOMu4BHAcM"
+    }
+  ],
+  "181": [
+    {
+      "title": "Debout les paras !",
+      "youtubeId": "q4c9Isg9wNw"
+    }
+  ],
+  "182": [
+    {
+      "title": "En passant par la portière",
+      "youtubeId": "sVr9uerYYRU"
+    }
+  ],
+  "183": [
+    {
+      "title": "La Galette",
+      "youtubeId": "3FymJcIhjxE"
+    }
+  ],
+  "185": [
+    {
+      "title": "Chant du 2ème REP",
+      "youtubeId": "i-GB7YSJGOM"
+    }
+  ],
+  "186": [
+    {
+      "title": "Pour nos morts",
+      "youtubeId": "4cP0trQnW1k"
+    }
+  ],
+  "187": [
+    {
+      "title": "Sous le soleil brûlant d'Afrique",
+      "youtubeId": "1YsMK4m83GQ"
+    }
+  ],
+  "188": [
+    {
+      "title": "Sidi-Brahim",
+      "youtubeId": "b6qfXz0z9_w"
+    }
+  ],
+  "189": [
+    {
+      "title": "Verdun ! On ne passe pas",
+      "youtubeId": "vRCg7R8m664"
+    }
+  ],
+  "190": [
+    {
+      "title": "L'Alsace et la Lorraine",
+      "youtubeId": "KK5s6I4nibg"
+    }
+  ],
+  "191": [
+    {
+      "title": "Marche des tirailleurs",
+      "youtubeId": "1vLLNAY_BV0"
+    }
+  ],
+  "192": [
+    {
+      "title": "L'Edelweiss",
+      "youtubeId": "rFVSt7KxqiE"
+    }
+  ],
+  "193": [
+    {
+      "title": "Hymne à Saint Michel",
+      "youtubeId": "DuPZqCi1xdo"
+    }
+  ],
+  "194": [
+    {
+      "title": "Les adieux suisses",
+      "youtubeId": "TVfBp7DkZs4"
+    }
+  ],
+  "195": [
+    {
+      "title": "Chant du légionnaire tombé",
+      "youtubeId": "Q-4m8fyQ6W4"
+    }
+  ],
+  "196": [
+    {
+      "title": "Cravate verte et képi blanc",
+      "youtubeId": "Lin81I5H4i4"
+    }
+  ],
+  "197": [
+    {
+      "title": "L'enfant de Strasbourg",
+      "youtubeId": "4meGLb8wV9g"
+    }
+  ],
+  "198": [
+    {
+      "title": "Sur la route",
+      "youtubeId": "qQgjmdSYVXg"
+    }
+  ],
+  "199": [
+    {
+      "title": "Le clairon",
+      "youtubeId": "O7k2qzIR-3s"
+    }
+  ],
+  "200": [
+    {
+      "title": "La légende du feu",
+      "youtubeId": "9waoLvmwNYE"
+    }
+  ],
+  "204": [
+    {
+      "title": "Le feu brille",
+      "youtubeId": "1TG9mv_IMpo"
+    }
+  ],
+  "205": [
+    {
+      "title": "La chèvre",
+      "youtubeId": "XhiaMbnHOQQ"
+    }
+  ],
+  "206": [
+    {
+      "title": "Colchiques dans les prés",
+      "youtubeId": "4NPfeU2o_Gw"
+    }
+  ],
+  "207": [
+    {
+      "title": "Dans les prisons de Nantes",
+      "youtubeId": "2e_4gHwIF9w"
+    }
+  ],
+  "208": [
+    {
+      "title": "Le petit âne gris",
+      "youtubeId": "zS7KQHB11dA"
+    }
+  ],
+  "209": [
+    {
+      "title": "Les cosaques",
+      "youtubeId": "Az2jwuDunn4"
+    }
+  ],
+  "210": [
+    {
+      "title": "Kazatchok",
+      "youtubeId": "yadlK2rgqHI"
+    }
+  ],
+  "211": [
+    {
+      "title": "Plaine, ma plaine",
+      "youtubeId": "Ipc0VCYhQqA"
+    }
+  ],
+  "212": [
+    {
+      "title": "L'homme de Cro-Magnon",
+      "youtubeId": "wTaWiu9XDbg"
+    }
+  ],
+  "214": [
+    {
+      "title": "Se canto (Devant ma fenêtre)",
+      "youtubeId": "f26jp_WZGjA"
+    }
+  ],
+  "216": [
+    {
+      "title": "Les crocodiles",
+      "youtubeId": "1T9b0cax6s4"
+    }
+  ],
+  "217": [
+    {
+      "title": "Cantique des étoiles",
+      "youtubeId": "TAeu5SeUexg"
+    }
+  ],
+  "219": [
+    {
+      "title": "Les cornemuses",
+      "youtubeId": "KdH2tWg2ypM"
+    },
+    {
+      "title": "Maudit sois-tu carillonneur",
+      "youtubeId": "oxQvNO2JawQ"
+    }
+  ],
+  "220": [
+    {
+      "title": "Les retrouvailles",
+      "youtubeId": "nBBPtww3lMU"
+    }
+  ],
+  "221": [
+    {
+      "title": "Hegoak",
+      "youtubeId": "NKKB2hbAkTg"
+    }
+  ],
+  "222": [
+    {
+      "title": "La montagne",
+      "youtubeId": "_pJya8MdVMk"
+    }
+  ],
+  "223": [
+    {
+      "title": "La jument de Michao",
+      "youtubeId": "aes401GQTEk"
+    }
+  ],
+  "224": [
+    {
+      "title": "Je t'aime ô ma Patrie",
+      "youtubeId": "7xRWECkxV_w"
+    }
+  ],
+  "225": [
+    {
+      "title": "L'enfant au cœur d'or",
+      "youtubeId": "tMiy4LTvQo8"
+    }
+  ],
+  "226": [
+    {
+      "title": "Ô douce France",
+      "youtubeId": "nAtJ89uZ500"
+    }
+  ],
+  "227": [
+    {
+      "title": "Le jour au loin s'enfuit",
+      "youtubeId": "Km7hyRF_aOQ"
+    }
+  ],
+  "228": [
+    {
+      "title": "Hymne à la joie",
+      "youtubeId": "wXRvISLN7MM"
+    }
+  ],
+  "229": [
+    {
+      "title": "Canon du feu",
+      "youtubeId": "SGzLfptioTE"
+    },
+    {
+      "title": "Canon de la pluie",
+      "youtubeId": "hVXtim3hhho"
+    }
+  ],
+  "231": [
+    {
+      "title": "Table des matières",
+      "youtubeId": "NONE"
+    }
+  ]
+};
